d2697fbc5a67cb694164f9926c8a3c0d60fc3650
ClemenceS <57844548+ClemenceS@users.noreply.github.com>
2019-11-17 16:21:30 +0100

    Update Environment.java

--- squelette1FRog/src/environment/Environment.java
+++ squelette1FRog/src/environment/Environment.java
@@ -7,6 +7,25 @@ import gameCommons.IEnvironment;
 
 public class Environment implements IEnvironment {
 		
-	//TODO
+	private ArrayList<Lane> line = new ArrayList<>();
+	private Game game;
+
+	//Construteurs
+	public Environment(Lane ligne) {
+		boolean sens;
+		for (int i = 0; i < game.height; i ++) {
+			//Crée un nb aléatoire entre 0 et 1 -> met false ou true pour connaitre le sens de la ligne
+			int alea = 0 + (int)(Math.random() * ((1 - 0) + 1));
+			if (alea == 0) {
+				sens =true;
+			} else {
+				sens  = false;
+			}
+			//crée une ligne
+			ligne = new Lane (game, i, sens, game.defaultDensity);
+			//add une ligne ds Array List. addLane
+			this.line.add(ligne);
+		} 
+	}
 
 }
